d098856eee888bced16ddc58972c2dac71840d7a
longhai <longhaiqwe@gmail.com>
2026-01-26 09:49:30 +0800

    Initial Commit

---
+++ RadioApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RadioApp
+//
+//  Created by longhai on 2026/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RadioApp/RadioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioAppApp.swift
+//  RadioApp
+//
+//  Created by longhai on 2026/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RadioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
